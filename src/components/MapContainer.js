4b59182b1989b8838af35667238068b87d53151f
Million Yemane <wedi_ngus@yahoo.com>
2018-05-17 15:55:09 -0500

    getting fillterrr

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,20 +1,26 @@
 import React, { Component } from "react";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import '../App.css';
 // import icon  from '../img/del.png';
 export class MapContainer extends Component {
     constructor(props) {
         super(props);
-        this.onMarkerClick = this.onMarkerClick.bind(this);
+
         this.state = {
             showingInfoWindow: false,
             activeMarker: {},
-            selectedPlace: {}
+            selectedPlace: {},
+            youName:null,
+            zipCode:null
+            lat:null,
+            lng:null,
         };
+         this.onMarkerChange = this.onMarkerChange.bind(this);
+          this.onMarkerClick = this.onMarkerClick.bind(this);
     }
     onMarkerClick(props, marker, e) {
         this.setState({
             selectedPlace: props,
-            center:{lat:36.174465, lng:-86.767960}
             activeMarker: marker,
             showingInfoWindow: true
         });
@@ -30,14 +36,13 @@ export class MapContainer extends Component {
         }
         return (
             <div>
-<<<<<<< HEAD
-                <Map style={style} google={this.props.google}zoom={5} className={"map"}>
-=======
-                <Map style={{minWidth: "100px",minHeight: "100px"}} google={this.props.google}zoom={14} className={"map"}>
->>>>>>> master
-                <Marker onClick={this.onMarkerClick} name={'Me and dan'}/>
 
-                    <Marker title={'The marker`s title will appear as a tooltip.'} onClick={this.onMarkerClick} name={'Dan in nashville'} position={{ lat: 36.1667, lng: -86.4660 }} />
+                <Map style={style} google={this.props.google}zoom={5} className={"map"}>
+
+
+
+
+                    <Marker  onClick={this.onMarkerClick} name={'I think Dan is in nashville'} position={{ lat:36.1667, lng:-86.4660 }}/>
                       <InfoWindow marker={this.state.activeMarker} visible={this.state.showingInfoWindow}>
                         <div>
                             <h1>{this.state.selectedPlace.name}</h1>
