71e0ed0f4ea96d87d021d915f9ad716aad72dc97
Million Yemane <wedi_ngus@yahoo.com>
2018-05-17 13:52:32 -0500

    fresh map

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-// import './App.css';
+import './App.css';
 import MapContainer from './components/MapContainer';
 import { Marker } from 'google-maps-react';
-// import Marker from './components/Marker';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Marker/>
-
         </header>
 <MapContainer/>
       </div>
