aa0ec0fb4fa2b7f862acc1c6ef626cb5d5cacc7e
Million Yemane <wedi_ngus@yahoo.com>
2018-05-21 12:05:42 -0500

    pulling down list data

--- src/App.js
+++ src/App.js
@@ -5,66 +5,12 @@ import Header from './components/Header';
 import FooterNav from './components/FooterNav';
 import Filter from './components/Filter';
 class App extends Component {
-  getByGeocoords(geoCoords, string){
-
-
-    return string;
-  }
-
-
-  getByCrime(crime, string){
-
-    return string;
-  }
-
-
-  getByTime(time, string){
-
-    return string;
-  }
-
-
-  runApi(){
-    //Series of methods for string builders activated here.
-    let string, time, crime, geoCoords;
-
-
-    if(true === (false)){ //conditional for switching....
-       string +=` ${this.getByTime(time, string)}`;
-       string +=` ${this.getByCrime(crime, string)}`;
-       string +=` ${this.getByGeocoords(geoCoords, string)}`;
-
-    }
-
-    // Change the fetch with modified string.
-
-    fetch('https://data.nashville.gov/resource/28i3-48zr.json?block=0',{
-      method: "GET",
-      data: {
-        "$limit" : 5000,
-        "$$app_token" : "r1zPUd6qffmC6asW1Y8pPPhuj"
-
-      },
-      header: {
-        "Access-Control-Allow-Origin" : "*"
-   }
-    }).then((data)=>{
-      data.json().then((resolved)=>{
-        console.log("Test returned ", resolved);
-      });
-
-    });
-
-  }
-
 
   render() {
     return (
       <div className="App">
         <Header />
-        <p className="App-intro" runapi={this.runApi()}>
-         Check console because that's the only place you'll see changes atm.
-        </p>
+      
         <PrimaryContainer />
         <FooterNav />
       </div>
