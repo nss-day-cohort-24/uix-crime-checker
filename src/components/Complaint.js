794d00ed8aac42da7cdb6da43e6e4c61bce63eb8
Kelsey Pintens <kelseypintens@Kelseys-MacBook-Pro.local>
2018-05-21 15:49:54 -0500

    working on submit form

--- src/components/Complaint.js
+++ src/components/Complaint.js
@@ -1,14 +1,83 @@
 import React, { Component } from 'react';
+import { FormGroup, Input, Label } from 'reactstrap';
 import './Filter.css';
 
 class ComplaintInput extends Component {
 
-      render() {
-        return (
-            <div>
-
-                <div>Suspicious Person</div>
-                <div>Mentally Ill Person</div>
+    constructor(props) {
+        super(props);
+        /* set the initial checkboxState to true */
+        this.state = {
+          checkboxState: true
+        }
+      }
+      /* prevent form submission from reloading the page */
+    onSubmit(event) {
+        event.preventDefault();
+      }
+      /* callback to change the checkboxState to false when the checkbox is checked */
+    toggle(event) {
+      this.setState({
+        checkboxState: !this.state.checkboxState
+      });
+    }
+    render() {
+      const checkedOrNot = [];
+      checkedOrNot.push(
+        console.log("Fucking checkbox"));
+      const checkbox = (
+        <span>
+          <input 
+          type="checkbox"
+          onClick={this.toggle.bind(this)}
+          />
+          <label>Checkbox</label>
+        </span>
+      );
+  
+      return (
+        <div>
+            <FormGroup check>
+            <Label check>
+                <Input type="checkbox" />{' '}
+                Suspicious Person
+            </Label>
+            </FormGroup>
+            <FormGroup check>
+            <Label check>
+                <Input type="checkbox" />{' '}
+                Intoxicated Person
+            </Label>
+            </FormGroup>
+            <FormGroup check>
+            <Label check>
+                <Input type="checkbox" />{' '}
+                Want Officer for Investigation
+            </Label>
+            </FormGroup>
+            <FormGroup check>
+            <Label check>
+                <Input type="checkbox" />{' '}
+                Disorderly Person
+            </Label>
+            </FormGroup>
+            <FormGroup check>
+            <Label check>
+                <Input type="checkbox" />{' '}
+                Theft
+            </Label>
+            </FormGroup>
+            <FormGroup check>
+            <Label check>
+                <Input type="checkbox" />{' '}
+                Cutting/Stabbing
+            </Label>
+            </FormGroup>
+        </div>
+      );
+    }
+  }
+                {/* <div>Suspicious Person</div>
                 <div>Intoxicated Person</div>
                 <div>Want Officer for Investigation</div>
                 <div>Disorderly Person</div>
@@ -28,7 +97,6 @@ class ComplaintInput extends Component {
                 <div>Burglary - Residence</div>
                 <div>Burglary - Non-Residence</div>
                 <div>Item / Vehicle is Stolen</div>
-                <div>Missing Person</div>
                 <div>Gang Activity</div>
                 <div>Occult Activity</div>
                 <div>Shots Fired</div>
@@ -39,12 +107,9 @@ class ComplaintInput extends Component {
                 <div>Personnel in Serious Danger</div>
                 <div>Riot/Disorderly Crowd</div>
                 <div>Explosion</div>
-                <div>Active Aggressor</div>
+                <div>Active Aggressor</div> */}
 
-            </div>
-        );
-    }
-}
+
 
 export default ComplaintInput;
 
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -7,6 +7,8 @@ import CollapseExample from './Collapse';
 import LocationInput from './Location';
 import DateInput from './Date';
 import ComplaintInput from './Complaint';
+import { Collapse, Button, CardBody, Card } from 'reactstrap';
+
  
 class Filter extends Component {
     constructor(props) {
@@ -32,13 +34,24 @@ class Filter extends Component {
                 from='left'
                 width='85%'
                 onRequestClose={ () => this.setState({ isPaneOpenLeft: false }) }>
-                <hr></hr>
-                <CollapseExample title="Type of Complaint" form={<ComplaintInput/>}/>
-                <hr></hr>
-                <CollapseExample title="Date of Crime" form={<DateInput/>}/>
-                <hr></hr>
-                <CollapseExample title="Location" form={<LocationInput/>}/>
-                <hr></hr>
+                <form onSubmit={this.handleSubmit}>
+                    <hr></hr>
+                    <CollapseExample title="Type of Complaint" form={<ComplaintInput/>}/>
+
+                                    {/* // <div>
+                                    //     <div className="filterBtn" onClick={this.toggle} style={{ marginBottom: '1rem' }}>{this.props.title}</div>
+                                    //     <Collapse isOpen={this.state.collapse}>
+                                    //         <div>{this.props.form}</div>
+                                    //     </Collapse>
+                                    // </div> */}
+
+                    <hr></hr>
+                    <CollapseExample title="Date of Crime" form={<DateInput/>}/>
+                    <hr></hr>
+                    <CollapseExample title="Location" form={<LocationInput/>}/>
+                    <hr></hr>
+                    <input type="submit" value="Submit" />
+                </form>
             </SlidingPane>
         </div>
     );
