fa8c84ae648411cb4ce02e3fbcae132e0e2a4dbd
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-05-22 09:33:46 -0500

    menu revisions. tried to overflow scroll the primary container but that needs work

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,28 +35,20 @@ class Header extends Component {
               <Collapse isOpen={this.state.isOpen} navbar>
                 <Nav className="ml-auto" navbar>
                   <NavItem>
-                    <NavLink href="/components/">Components</NavLink>
+                    <NavLink href="https://xd.adobe.com/view/ac66109f-fd30-4707-4486-17c2e2dd1d45-da57/" target="_blank">Group xD File</NavLink>
                   </NavItem>
                   <NavItem>
-                    <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                    <NavLink href="https://github.com/nss-day-cohort-24/uix-crime-checker" target="_blank">Crime Checker GitHub</NavLink>
                   </NavItem>
-                  <UncontrolledDropdown nav inNavbar>
-                    <DropdownToggle nav caret>
-                      Options
-                    </DropdownToggle>
-                    <DropdownMenu right>
-                      <DropdownItem>
-                        Option 1
-                      </DropdownItem>
-                      <DropdownItem>
-                        Option 2
-                      </DropdownItem>
-                      <DropdownItem divider />
-                      <DropdownItem>
-                        Reset
-                      </DropdownItem>
-                    </DropdownMenu>
-                  </UncontrolledDropdown>
+                  <NavItem>
+                    <NavLink href="/">Additional Resources 1</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/">Additional Resources 2</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/">Additional Resources 3</NavLink>
+                  </NavItem>
                 </Nav>
               </Collapse>
             </Navbar>
