ada2fdbacd57838de6f07c52d8b4c71b0be1a612
Dan Watson <danwatson00@yahoo.com>
2018-05-22 15:24:47 -0500

    changed to 500 data points from 5000

--- src/components/PrimaryContainer.js
+++ src/components/PrimaryContainer.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './PrimaryContainer.css';
 import MapContainer from './MapContainer';
 import Filter from './Filter';
@@ -7,93 +7,93 @@ import ListData from './ListData';
 // console.log('hi mu mu')
 
 class PrimaryContainer extends Component {
-// console.log('hi mu mu');
-  constructor(props) {
-super(props);
- this.state = {
-   objResult:[],
-   policeLoaded:false,
-   error:null,
- }
- this.getPolice=this.getPolice.bind(this);
-//  this.handleChange = this.handleChange.bind(this);
-//  this.handlesubmitpro = this.handlesubmitpro.bind(this);
-}
+    // console.log('hi mu mu');
+    constructor(props) {
+        super(props);
+        this.state = {
+            objResult: [],
+            policeLoaded: false,
+            error: null,
+        }
+        this.getPolice = this.getPolice.bind(this);
+        //  this.handleChange = this.handleChange.bind(this);
+        //  this.handlesubmitpro = this.handlesubmitpro.bind(this);
+    }
 
 
-    getPolice(){
+    getPolice() {
 
-        fetch(`https://data.nashville.gov/resource/28i3-48zr.json?block=0`,{
-        method: "GET",
-        data: {
-          "$limit" : 5000,
-          "$$app_token" : "r1zPUd6qffmC6asW1Y8pPPhuj"
-        },
-         header: {
-        "Access-Control-Allow-Origin" : "*"
-   }
+        fetch(`https://data.nashville.gov/resource/28i3-48zr.json?block=0`, {
+            method: "GET",
+            data: {
+                "$limit": 100,
+                "$$app_token": "r1zPUd6qffmC6asW1Y8pPPhuj"
+            },
+            header: {
+                "Access-Control-Allow-Origin": "*"
+            }
         }).then((result) => {
-            console.log("my result",result);
+            console.log("my result", result);
             result.json().then((resolved) => {
                 // console.log("Test first return ", resolved);
-                let objResults=[];
-               resolved.map((item, index)=>{
-                   objResults.push({
-                         block: item.block,
-                         call_received:item.call_received,
-                         complaint:item.complaint,
-                         description:item.description,
-                         disposition_code:item.disposition_code,
-                         disposition_description:item.disposition_description,
-                         event_number:item.event_number,
-                         latitude:item.latitude,
-                         longitude:item.longitude,
-                         mapped_location:item.mapped_location,
-                         rpa:item.rpa,
-                         sector:item.sector,
-                         shift:item.shift,
-                         street_name:item.street_name,
-                         tencode:item.tencode,
-                         tencode_suffix:item.tencode_suffix,
-                         tencode_suffix_description:item.tencode_suffix_description,
-                         unit_dispatched:item.unit_dispatched,
-                         zone:item.zone,
+                let objResults = [];
+                resolved.map((item, index) => {
+                    objResults.push({
+                        block: item.block,
+                        call_received: item.call_received,
+                        complaint: item.complaint,
+                        description: item.description,
+                        disposition_code: item.disposition_code,
+                        disposition_description: item.disposition_description,
+                        event_number: item.event_number,
+                        latitude: item.latitude,
+                        longitude: item.longitude,
+                        mapped_location: item.mapped_location,
+                        rpa: item.rpa,
+                        sector: item.sector,
+                        shift: item.shift,
+                        street_name: item.street_name,
+                        tencode: item.tencode,
+                        tencode_suffix: item.tencode_suffix,
+                        tencode_suffix_description: item.tencode_suffix_description,
+                        unit_dispatched: item.unit_dispatched,
+                        zone: item.zone,
 
                     })
                 });
                 console.log("above the set state ", objResults);
-        this.setState({
-                objResult:objResults,
-                policeLoaded: true,
-              
-                    });
+                this.setState({
+                    objResult: objResults,
+                    policeLoaded: true,
+
+                });
 
             });
         });
 
-          }
+    }
     componentDidMount() {
         this.getPolice();
     }
-  render(){
- 
-              return(
-                  <div className="App">
-                      <div className="filter">
-                          <Filter />
-                      </div>
-                      <div className="map">
-                         </div>
-                      <div className="listDiv">
-                        {/* {this.state.objResult} */}
-                      </div>
-                      <ListData />
-                      <div className="footerNav">
-                          <p>This is the container that will set state for whether to view map or list. </p>
-                      </div>
+    render() {
+
+        return (
+            <div className="App">
+                <div className="filter">
+                    <Filter />
+                </div>
+                <div className="map">
+                </div>
+                <div className="listDiv">
+                    {/* {this.state.objResult} */}
+                </div>
+                <ListData />
+                <div className="footerNav">
+                    <p>This is the container that will set state for whether to view map or list. </p>
+                </div>
 
 
-                  </div>
+            </div>
         );
     }
 }
